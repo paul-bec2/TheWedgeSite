faee0bcf5d461aeb5df75bc1e28293b42a62641a
Paul Bechu <paul123bec@gmail.com>
2024-03-18 11:56:02 -0500

    First try at the template, nothing appearing but compiling successfully

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
+
+import LandingPage from "./LandingPage";
+
+import "../node_modules/bootstrap/scss/bootstrap.scss";
+// import "./assets/scss/paper-kit.scss?v=1.3.0";
+// import "./assets/demo/demo.css?v=1.3.0";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter basename="/App">
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
